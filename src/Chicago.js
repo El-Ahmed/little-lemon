b72af54b099edf3764ddbc6f3e9db7cc793b310a
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 11:42:05 +0100

    add chicago section

---
+++ src/Chicago.js
@@ -0,0 +1,19 @@
+
+function Chicago() {
+    return (
+        <section className="hero-section chicago">
+            <div>
+                <div className="hero-text">
+                    <h1>Little Lemon</h1>
+                    <h2>Chicago</h2>
+                    <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+                </div>
+                <img src="restaurant.jpg" width={300} height={400} alt="restaurant" className="restaurant" />
+                <img src="restaurant chef B.jpg" width={300} height={400} alt="restaurant chef" className="restaurant-chef" />
+            </div>
+        </section>
+    )
+}
+
+export default Chicago;
