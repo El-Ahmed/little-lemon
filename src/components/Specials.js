272645265b6fbbe6d948f7522802de02171bfa66
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 12:11:57 +0100

    put static lists outside the functions

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,22 +1,27 @@
+import { Link } from "react-router-dom";
 
-function Specials() {
-    const specials = [{
+const specials = [
+    {
         title: 'Greek salad',
         description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
         price: '12.99',
         image: 'greek salad.jpg'
-    }, {
+    },
+    {
         title: 'Bruchetta',
         description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
         price: '5.99',
         image: 'bruchetta.jpg'
-    }, {
+    },
+    {
         title: 'Lemon Dessert',
         description: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
         price: '5.00',
         image: 'lemon dessert.jpg'
     }
-    ]
+]
+
+function Specials() {
     return (
         <section>
             <div>
@@ -37,7 +42,7 @@ function Specials() {
                             <p>
                                 {special.description}
                             </p>
-                            <a href="/">Order a delivery</a>
+                            <Link to="/">Order a delivery</Link>
                         </div>
                     </article>
                 ))}
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,27 +1,27 @@
-
+const testimonials = [
+    {
+        rating: 5,
+        name: "Emma Johnson",
+        text: "The food at Little Lemon is always fresh and full of flavor. I can’t get enough of their dishes!",
+    },
+    {
+        rating: 4,
+        name: "Michael Brown",
+        text: "Cozy atmosphere and friendly staff. A perfect place to enjoy a relaxed meal with friends.",
+    },
+    {
+        rating: 5,
+        name: "Sophia Lee",
+        text: "Little Lemon never disappoints. Delicious meals and great service every time I visit.",
+    },
+    {
+        rating: 4,
+        name: "James Smith",
+        text: "I love the variety of dishes and the freshness of the ingredients. Highly recommend Little Lemon!",
+    },
+];
 function Testimonials() {
-    const testimonials = [
-        {
-            rating: 5,
-            name: "Emma Johnson",
-            text: "The food at Little Lemon is always fresh and full of flavor. I can’t get enough of their dishes!",
-        },
-        {
-            rating: 4,
-            name: "Michael Brown",
-            text: "Cozy atmosphere and friendly staff. A perfect place to enjoy a relaxed meal with friends.",
-        },
-        {
-            rating: 5,
-            name: "Sophia Lee",
-            text: "Little Lemon never disappoints. Delicious meals and great service every time I visit.",
-        },
-        {
-            rating: 4,
-            name: "James Smith",
-            text: "I love the variety of dishes and the freshness of the ingredients. Highly recommend Little Lemon!",
-        },
-    ];
+
     return (
         <section className="testimonials">
             <div>
