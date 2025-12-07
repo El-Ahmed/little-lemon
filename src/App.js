339de2fee0e57cc06187c31745d808ee134950f0
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 22:49:17 +0100

    configure github pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,11 +6,11 @@ import Main from './Main';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Main />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
