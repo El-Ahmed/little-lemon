c9c3ee6f66c7d45b30df922f846796382844e277
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 20:55:12 +0100

    improve accessibility in booking form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -28,23 +28,70 @@ function BookingForm({ availableTimes, dispatch, submitBooking }) {
     return (
         <form onSubmit={handleSubmit} >
             <label htmlFor="res-date">Choose date</label>
-            <input className={errors.date ? 'error' : ''} required min={new Date().toISOString().split('T')[0]} type="date" id="res-date" name="date" value={values.date} onChange={handleDateChange} />
-            {errors.date ? <div className="error">{errors.date}</div> : null}
+            <input
+                className={errors.date ? 'error' : ''}
+                required
+                aria-required="true"
+                min={new Date().toISOString().split('T')[0]}
+                type="date"
+                id="res-date"
+                name="date"
+                value={values.date}
+                onChange={handleDateChange}
+                aria-describedby={errors.date ? 'date-error' : undefined}
+            />
+            {errors.date ? <div id="date-error" className="error">{errors.date}</div> : null}
             <label htmlFor="res-time" >Choose time</label>
-            <select id="res-time" name="time" required value={values.time} onChange={handleChange}>
+            <select
+                id="res-time"
+                name="time"
+                required
+                value={values.time}
+                aria-required="true"
+                aria-describedby={errors.time ? 'time-error' : undefined}
+                onChange={handleChange}
+            >
                 {availableTimes.map(availableTime => (
                     <option key={availableTime} value={availableTime}>{availableTime}</option>
                 ))}
             </select>
+            {errors.time ? <div id="time-error" className="error">{errors.time}</div> : null}
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" required name="occasion" value={values.occasion} onChange={handleChange}>
+            <select
+                id="occasion"
+                required
+                aria-required="true"
+                name="occasion"
+                value={values.occasion}
+                onChange={handleChange}
+                aria-describedby={errors.time ? 'occasion-error' : undefined}
+            >
                 <option value={'Birthday'}>Birthday</option>
                 <option value={'Anniversary'}>Anniversary</option>
             </select>
+            {errors.occasion ? <div id="occasion-error" className="error">{errors.occasion}</div> : null}
             <label htmlFor="guests">Number of guests</label>
-            <input className={errors.guests ? 'error' : ''} required name="guests" type="number" placeholder="1" min="1" max="10" id="guests" value={values.guests} onChange={handleChange} />
-            {errors.guests ? <div className="error">{errors.guests}</div> : null}
-            <input type="submit" className="button" value="Make Your reservation" />
+            <input
+                className={errors.guests ? 'error' : ''}
+                required
+                aria-required="true"
+                name="guests"
+                type="number"
+                placeholder="1"
+                min="1"
+                max="10"
+                id="guests"
+                value={values.guests}
+                onChange={handleChange}
+                aria-describedby={errors.time ? 'guests-error' : undefined}
+            />
+            {errors.guests ? <div id="guests-error" className="error">{errors.guests}</div> : null}
+            <input
+                type="submit"
+                className="button"
+                value="Make Your reservation"
+                aria-label="On Click"
+            />
         </form>
     )
 }
