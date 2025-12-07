353ffd89e2e5d7c76df528fbce388b172b2cb292
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 22:39:40 +0100

    add menu button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
 
-function Nav() {
+function Nav({ onLinkClick }) {
     return (
         <nav aria-label="Site navigation">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/menu">Menu</Link></li>
-                <li><Link to="/booking">Reservations</Link></li>
-                <li><Link to="/order-online">Order Online</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/" onClick={onLinkClick}>Home</Link></li>
+                <li><Link to="/about" onClick={onLinkClick}>About</Link></li>
+                <li><Link to="/menu" onClick={onLinkClick}>Menu</Link></li>
+                <li><Link to="/booking" onClick={onLinkClick}>Reservations</Link></li>
+                <li><Link to="/order-online" onClick={onLinkClick}>Order Online</Link></li>
+                <li><Link to="/login" onClick={onLinkClick}>Login</Link></li>
             </ul>
         </nav>
     );
