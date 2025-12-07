97942e9825e8906b3884f11aa43e1d87cd18eafd
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 16:09:39 +0100

    update unit tests

--- src/tests/bookingReducer.test.js
+++ src/tests/bookingReducer.test.js
@@ -1,11 +1,40 @@
 import { initializeTimes, updateTimes } from "../utils/bookingReducer";
 
 test('initializeTimes returns correct initial times', () => {
+    jest.useFakeTimers()
+    jest.setSystemTime(new Date('2025-12-06'));
     const times = initializeTimes();
-    expect(times).toEqual([]);
+    expect(times).toEqual([
+        "17:00",
+        "17:30",
+        '20:00',
+        '20:30',
+        "22:30",
+        "23:00",
+        "23:30",
+    ]);
 });
 
 test('updateTimes returns correct times when date changes', () => {
-    const newState = updateTimes([], { date: '2025-12-07' });
-    expect(newState).toEqual(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+    const initialTimes = [
+        "17:00",
+        "17:30",
+        '20:00',
+        '20:30',
+        "22:30",
+        "23:00",
+        "23:30",
+    ]
+
+    const expectedTimes = [
+        "17:00",
+        "17:30",
+        "19:00",
+        "22:30",
+        "23:00",
+        "23:30"
+    ]
+
+    const newState = updateTimes(initialTimes, { date: '2025-12-07' });
+    expect(newState).toEqual(expectedTimes);
 });
