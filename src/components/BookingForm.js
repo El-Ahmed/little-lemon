bb7afee1fdd3ef530c1bc29843000cf011c4acea
Ahmed El herz <elherzahmed@gmail.com>
2025-12-07 14:15:05 +0100

    add states to booking form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,25 +1,28 @@
+import { useState } from "react";
 
 function BookingForm() {
+    const [date, setDate] = useState()
+    const [time, setTime] = useState()
+    const [occasion, setOccasion] = useState()
+    const [guests, setGuests] = useState(1)
+    const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'])
     return (
         <form >
             <label for="res-date">Choose date</label>
-            <input type="date" id="res-date" />
-            <label for="res-time">Choose time</label>
-            <select id="res-time ">
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
+            <input type="date" id="res-date" value={date} onChange={e => setDate(e.target.value)} />
+            <label for="res-time" >Choose time</label>
+            <select id="res-time" value={time} onChange={e => setTime(e.target.value)}>
+                {availableTimes.map(availableTime => (
+                    <option key={availableTime} value={availableTime}>{availableTime}</option>
+                ))}
             </select>
             <label for="occasion">Occasion</label>
-            <select id="occasion">
-                <option>Birthday</option>
-                <option>Anniversary</option>
+            <select id="occasion" value={occasion} onChange={e => setOccasion(e.target.value)}>
+                <option value={'Birthday'}>Birthday</option>
+                <option value={'Anniversary'}>Anniversary</option>
             </select>
             <label for="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" />
+            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={e => setGuests(e.target.value)} />
             <input type="submit" className="button" value="Make Your reservation" />
         </form>
     )
